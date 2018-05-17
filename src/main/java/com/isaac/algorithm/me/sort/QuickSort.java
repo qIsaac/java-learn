f8e4dda5b743875134263592634c9428de92930c
isaac <isaacqc@outlook.com>
2018-05-17 23:08:05 +0800

    commit quicksort

--- src/main/java/com/isaac/algorithm/me/sort/QuickSort.java
+++ src/main/java/com/isaac/algorithm/me/sort/QuickSort.java
@@ -6,5 +6,34 @@ package com.isaac.algorithm.me.sort;
  * @Date :Created in 11:37 2018/5/17
  */
 public class QuickSort {
+    public static void  sort(Comparable[] arr) {
+        int n = arr.length;
+        sort(arr, 0, n-1);
+    }
 
+    private static void sort(Comparable[] arr, int l, int r) {
+        if ( l >= r){
+            return;
+        }
+        int p = partition(arr, l, r);
+        sort(arr, l, p-1);
+        sort(arr, p+1, r);
+    }
+
+    private static int partition(Comparable[] arr, int l, int r) {
+        Comparable v = arr[l];
+        int j = l;
+        for (int i = l+1; i <= r ; i++) {
+            if (arr[i].compareTo(v) < 0) {
+                swap(arr, ++j ,i);
+            }
+        }
+        swap(arr,l,j);
+        return j;
+    }
+    public static void swap(Object[] arr, int i, int j) {
+        Object temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
